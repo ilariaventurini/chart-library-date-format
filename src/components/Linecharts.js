c5a6f2ec8280b8a20a3a5691306fe2e57a5b2d2d
Ilaria Venturini <ilaria.venturini@accurat.it>
2020-03-02 16:55:25 +0100

    Add data from csv

--- src/components/Linecharts.js
+++ src/components/Linecharts.js
@@ -4,12 +4,14 @@ import appearanceConfig from '../lib/appearanceConfig.json'
 import { dimensions } from '../lib/constants'
 import { dataJuggler } from '../lib/fixDJ'
 import { d1, d2 } from '../datasets/datasets'
+import d3 from '../datasets/data.csv'
 
 // call the method `dataJuggler` for each dataset
 // the first input is the dataset (d1, d2, ...)
 // the second input is the name of the date column
 const { data: data1, moments: moments1, types: types1 } = dataJuggler(d1, 'date')
 const { data: data2, moments: moments2, types: types2 } = dataJuggler(d2, 'birthday')
+const { data: data3, moments: moments3, types: types3 } = dataJuggler(d3, 'date')
 
 export class Linecharts extends React.Component {
   render() {
@@ -42,6 +44,20 @@ export class Linecharts extends React.Component {
           appearanceConfig={appearanceConfig}
           dimensions={dimensions}
           types={types2}
+          withLegend={true}
+        />
+
+        <Linechart
+          data={data3}
+          moments={moments3}
+          structure={{
+            x: [{ variable: 'date' }],
+            y: [{ variable: 'value' }],
+            color: [{ variable: 'cat' }],
+          }}
+          appearanceConfig={appearanceConfig}
+          dimensions={dimensions}
+          types={types3}
           withLegend={true}
         />
       </div>
